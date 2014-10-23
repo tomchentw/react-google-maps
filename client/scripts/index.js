543f46723fb4fea536ba5f5a628f332a66f7247a
Tom Chen <developer@tomchentw.com>
2014-10-23 20:02:20 +0800

    chore(client): fix index.html for google maps script loading
    * also add demo Body component

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,2 +1,44 @@
 /** @jsx React.DOM */
+"use strict";
 require("../styles/index.scss");
+var React = require("react/addons");
+
+var {GoogleMap} = require("../../src");
+
+var Body = React.createClass({
+
+  getInitialState () {
+    var googleMapsApi = google.maps;
+    return {
+      googleMapsApi,
+      mapOptions: {
+        center: {
+          lat: -34.397,
+          lng: 150.644
+        },
+        zoom: 8,
+        mapTypeId: googleMapsApi.MapTypeId.ROADMAP
+      }
+    };
+  },
+
+  googleMapsApiLoaded () {
+    this.setState({
+      googleMapsApi: google.maps
+    });
+  },
+
+  render () {
+    return <div>
+      <GoogleMap
+        googleMapsApi={this.state.googleMapsApi}
+        mapOptions={this.state.mapOptions}>
+      </GoogleMap>
+    </div>
+  }
+});
+
+var bodyRef = React.renderComponent(
+  <Body />,
+  document.getElementById("react-root")
+);

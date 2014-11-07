3e9f8551c56dc3849594f88b5ddd3d706b083aaf
Tom Chen <developer@tomchentw.com>
2014-11-07 08:49:29 +0800

    chore(webpack.config.js): add __template__gist__ and update style

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,13 +1,17 @@
-var Path = require("path");
-var webpack = require("webpack");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-
-var JSX_LOADER = "jsx-loader?harmony";
-var SCSS_LOADER = "style-loader!css-loader?root=../!sass-loader?includePaths[]=" +
+/**
+ * __template__gist__: https://gist.github.com/tomchentw/368a93bb748ad9d576f1#file-webpack-config-js
+ */
+var Path = require("path"),
+    webpack = require("webpack"),
+    HtmlWebpackPlugin = require("html-webpack-plugin"),
+    webpackConfig,
+ 
+    IS_PRODUCTION = "production" === process.env.NODE_ENV,
+    JSX_LOADER = "jsx-loader?harmony",
+    SCSS_LOADER = "style-loader!css-loader?root=../!sass-loader?includePaths[]=" +
         Path.resolve(__dirname, "../bower_components/bootstrap-sass-official/assets/stylesheets");
-var IS_PRODUCTION = "production" === process.env.NODE_ENV;
-
-var webpackConfig = module.exports = {
+ 
+webpackConfig = module.exports = {
   entry: "./client/scripts/index.js",
   output: {
     path: Path.resolve(__dirname, "../public/assets"),
@@ -33,7 +37,7 @@ var webpackConfig = module.exports = {
     })
   ]
 };
-
+ 
 if (IS_PRODUCTION) {
   webpackConfig.plugins.push(
     new webpack.optimize.DedupePlugin()

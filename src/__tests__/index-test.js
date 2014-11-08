f06bd13eb3c4164dc64aa65454c99086557c4f84
Tom Chen <developer@tomchentw.com>
2014-11-08 16:05:02 +0800

    test(Map): fix test

--- src/__tests__/index-test.js
+++ src/__tests__/index-test.js
@@ -1,6 +1,7 @@
 "use strict";
 
 jest.dontMock("../index.js");
+jest.dontMock("../helpers/create_child_component");
 describe("index", function() {
   it("should export components", function() {
     var index = require("../index.js");

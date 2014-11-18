0add2c32f548963fe91a83969efbb9160bf16653
Tom Chen <developer@tomchentw.com>
2014-11-18 21:04:06 +0800

    chore(AsynchronousLoading): add client code that renders AsynchronousLoading

--- client/scripts/components/basics/AsynchronousLoading.js
+++ client/scripts/components/basics/AsynchronousLoading.js
@@ -1,13 +1,13 @@
 "use strict";
 var React = require("react/addons"),
 
-    {GoogleMapsMixin, Map} = require("react-google-maps");
-
-module.exports = React.createClass({
-  /*
-   * https://developers.google.com/maps/documentation/javascript/examples/map-simple-async
-   */
-  displayName: "SimpleMap",
+    {GoogleMapsMixin, Map} = require("react-google-maps"),
+    AsynchronousLoading;
+/*
+ * https://developers.google.com/maps/documentation/javascript/examples/map-simple-async
+ */
+AsynchronousLoading = React.createClass({
+  displayName: "AsynchronousLoading",
 
   mixins: [require("../../ReactFutureMixin"), GoogleMapsMixin],
 
@@ -15,5 +15,26 @@ module.exports = React.createClass({
     return <div style={{height: "100%"}} {...props}>
       <Map style={{height: "100%"}} zoom={8} center={{lat: -34.397, lng: 150.644}} />
     </div>;
+  }
+});
+
+module.exports = React.createClass({
+  mixins: [require("../../ReactFutureMixin")],
+
+  getInitialState () {
+    return {
+      googleMapsApi: null,
+    };
+  },
+
+  componentDidMount () {
+    setTimeout(() => {
+      // Emulated google.maps script is loaded async.
+      this.setState({googleMapsApi: google.maps});
+    }, 3000);
+  },
+
+  _render (props, state) {
+    return <AsynchronousLoading googleMapsApi={state.googleMapsApi} {...props} />;
   }
 });

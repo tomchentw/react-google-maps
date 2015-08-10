10ff7152af386e3d5d278d9106f4d26237706c09
Tom Chen <developer@tomchentw.com>
2015-08-10 20:22:26 +0800

    chore(examples): fix StyledMap example
    * Closes #97 and thanks to @samuelsantia

--- examples/gh-pages/scripts/components/basics/StyledMap.js
+++ examples/gh-pages/scripts/components/basics/StyledMap.js
@@ -8,100 +8,98 @@ import {default as InfoBox} from "../../../../../src/addons/InfoBox";
  */
 export default class StyledMap extends Component {
 
-  static defaultProps = {
+  static fancyMapStyles = [{
     // The style is copy from https://snazzymaps.com/style/2/midnight-commander
-    mapStyles: [{
-      "featureType": "all",
-      "elementType": "labels.text.fill",
-      "stylers": [{
-        "color": "#ffffff"
-      }]
-    }, {
-      "featureType": "all",
-      "elementType": "labels.text.stroke",
-      "stylers": [{
-        "color": "#000000"
-      }, {
-        "lightness": 13
-      }]
-    }, {
-      "featureType": "administrative",
-      "elementType": "geometry.fill",
-      "stylers": [{
-        "color": "#000000"
-      }]
-    }, {
-      "featureType": "administrative",
-      "elementType": "geometry.stroke",
-      "stylers": [{
-        "color": "#144b53"
-      }, {
-        "lightness": 14
-      }, {
-        "weight": 1.4
-      }]
-    }, {
-      "featureType": "landscape",
-      "elementType": "all",
-      "stylers": [{
-        "color": "#08304b"
-      }]
-    }, {
-      "featureType": "poi",
-      "elementType": "geometry",
-      "stylers": [{
-        "color": "#0c4152"
-      }, {
-        "lightness": 5
-      }]
-    }, {
-      "featureType": "road.highway",
-      "elementType": "geometry.fill",
-      "stylers": [{
-        "color": "#000000"
-      }]
-    }, {
-      "featureType": "road.highway",
-      "elementType": "geometry.stroke",
-      "stylers": [{
-        "color": "#0b434f"
-      }, {
-        "lightness": 25
-      }]
-    }, {
-      "featureType": "road.arterial",
-      "elementType": "geometry.fill",
-      "stylers": [{
-        "color": "#000000"
-      }]
-    }, {
-      "featureType": "road.arterial",
-      "elementType": "geometry.stroke",
-      "stylers": [{
-        "color": "#0b3d51"
-      }, {
-        "lightness": 16
-      }]
-    }, {
-      "featureType": "road.local",
-      "elementType": "geometry",
-      "stylers": [{
-        "color": "#000000"
-      }]
-    }, {
-      "featureType": "transit",
-      "elementType": "all",
-      "stylers": [{
-        "color": "#146474"
-      }]
-    }, {
-      "featureType": "water",
-      "elementType": "all",
-      "stylers": [{
-        "color": "#021019"
-      }]
+    "featureType": "all",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+      "color": "#ffffff"
     }]
-  }
+  }, {
+    "featureType": "all",
+    "elementType": "labels.text.stroke",
+    "stylers": [{
+      "color": "#000000"
+    }, {
+      "lightness": 13
+    }]
+  }, {
+    "featureType": "administrative",
+    "elementType": "geometry.fill",
+    "stylers": [{
+      "color": "#000000"
+    }]
+  }, {
+    "featureType": "administrative",
+    "elementType": "geometry.stroke",
+    "stylers": [{
+      "color": "#144b53"
+    }, {
+      "lightness": 14
+    }, {
+      "weight": 1.4
+    }]
+  }, {
+    "featureType": "landscape",
+    "elementType": "all",
+    "stylers": [{
+      "color": "#08304b"
+    }]
+  }, {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#0c4152"
+    }, {
+      "lightness": 5
+    }]
+  }, {
+    "featureType": "road.highway",
+    "elementType": "geometry.fill",
+    "stylers": [{
+      "color": "#000000"
+    }]
+  }, {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [{
+      "color": "#0b434f"
+    }, {
+      "lightness": 25
+    }]
+  }, {
+    "featureType": "road.arterial",
+    "elementType": "geometry.fill",
+    "stylers": [{
+      "color": "#000000"
+    }]
+  }, {
+    "featureType": "road.arterial",
+    "elementType": "geometry.stroke",
+    "stylers": [{
+      "color": "#0b3d51"
+    }, {
+      "lightness": 16
+    }]
+  }, {
+    "featureType": "road.local",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#000000"
+    }]
+  }, {
+    "featureType": "transit",
+    "elementType": "all",
+    "stylers": [{
+      "color": "#146474"
+    }]
+  }, {
+    "featureType": "water",
+    "elementType": "all",
+    "stylers": [{
+      "color": "#021019"
+    }]
+  }]
 
   _click_from_children_of_infoBox = (e) => {
     console.log("_click_from_children_of_infoBox!!");
@@ -109,20 +107,21 @@ export default class StyledMap extends Component {
   }
 
   render () {
-    const {props, state} = this,
-          {googleMapsApi, mapStyles, ...otherProps} = props;
     const myLatLng = new google.maps.LatLng(25.03, 121.6);
 
     return (
       <GoogleMap containerProps={{
-          ...otherProps,
+          ...this.props,
           style: {
             height: "100%",
           },
         }}
-        styles={mapStyles}
         defaultZoom={5}
-        defaultCenter={myLatLng}>
+        defaultCenter={myLatLng}
+        defaultOptions={{
+          styles: StyledMap.fancyMapStyles,
+        }}
+      >
         <InfoBox
           defaultPosition={myLatLng}
           options={{closeBoxURL: "", enableEventPropagation: true}}

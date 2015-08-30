b60785042cd80c6365b1d4fdb1214cf68bc9ee83
Tom Chen <developer@tomchentw.com>
2015-08-30 16:09:39 +0800

    docs(simplemap): switch to use reacthtmlpack

---
+++ examples/simplemap/Client.webpackConfig.js
@@ -0,0 +1,68 @@
+"use strict";
+
+var Path = require("path");
+var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var JSX_LOADER_LIST;
+var FILENAME_FORMAT;
+var PRODUCTION_PLUGINS;
+
+if ("production" === process.env.NODE_ENV) {
+  JSX_LOADER_LIST = ["babel"];
+  FILENAME_FORMAT = "[name]-[chunkhash].js";
+  PRODUCTION_PLUGINS = [
+    // Safe effect as webpack -p
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+  ];
+} else {
+  // For webpack-dev-server and HMR!!!
+  JSX_LOADER_LIST = ["react-hot", "babel"];
+  // When HMR is enabled, chunkhash cannot be used.
+  FILENAME_FORMAT = "[name].js";
+  PRODUCTION_PLUGINS = [];
+}
+
+module.exports = {
+  devServer: {
+    port: 8080,
+    host: "localhost",
+    "contentBase": Path.resolve(__dirname, "../../public"),
+    hot: true,
+  },
+  context: __dirname,
+  output: {
+    path: Path.resolve(__dirname, "../../public"),
+    filename: FILENAME_FORMAT,
+  },
+  resolve: {
+    alias: {
+      "react": Path.resolve(__dirname, "./node_modules/react"),
+    },
+  },
+  resolveLoader: {
+    root: Path.resolve(__dirname, "./node_modules")
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract("style", "css!sass", {
+          publicPath: ""
+        }),
+      },
+      {
+        test: /\.js(x?)$/,
+        exclude: /node_modules/,
+        loaders: JSX_LOADER_LIST,
+      },
+    ],
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin("NODE_ENV"),
+    new ExtractTextPlugin("[name]-[chunkhash].css", {
+      disable: "production" !== process.env.NODE_ENV
+    }),
+  ].concat(PRODUCTION_PLUGINS),
+};
---
+++ examples/simplemap/scripts/index.html.js
@@ -0,0 +1,37 @@
+import {
+  default as React,
+} from "react";
+
+import {
+  WebpackScriptEntry,
+  WebpackStyleEntry,
+  ReactRenderToStringEntry,
+} from "reacthtmlpack/lib/entry";
+
+export default (
+  <html>
+    <head>
+      <title>React Google Maps | tomchentw</title>
+      <WebpackStyleEntry
+        chunkName="assets/client"
+        chunkFilepath="./scripts/client.js"
+        configFilepath="../Client.webpackConfig.js"
+      />
+      <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing" />
+    </head>
+    <body>
+      <ReactRenderToStringEntry
+        id="react-container"
+        tagName="div"
+        chunkName="tmp/server"
+        chunkFilepath="./scripts/server.js"
+        configFilepath="../Server.webpackConfig.js"
+      />
+      <WebpackScriptEntry
+        chunkName="assets/client"
+        chunkFilepath="./scripts/client.js"
+        configFilepath="../Client.webpackConfig.js"
+      />
+    </body>
+  </html>
+);

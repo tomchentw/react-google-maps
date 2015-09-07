a615dc2a3d4f388b474f1b0766f9b43952bcc0e8
Tom Chen <developer@tomchentw.com>
2015-09-07 23:55:14 +0800

    docs(simplemap): add required meta tags

--- examples/simplemap/views/index.html.js
+++ examples/simplemap/views/index.html.js
@@ -12,6 +12,8 @@ export default (
   <html>
     <head>
       <title>React Google Maps | tomchentw</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <meta charSet="UTF-8" />
       <WebpackStyleEntry
         chunkName="assets/client"
         chunkFilepath="./scripts/client.js"

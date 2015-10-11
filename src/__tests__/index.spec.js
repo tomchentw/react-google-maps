e3aa5ec9874971fca2df97123b99a6927af77f96
Tom Chen <developer@tomchentw.com>
2015-10-11 16:07:34 +0800

    test(index.spec): add simple exported spec

---
+++ src/__tests__/index.spec.js
@@ -0,0 +1,35 @@
+import {
+  default as expect,
+} from "expect";
+
+import {
+  GoogleMap,
+
+  Circle,
+  DirectionsRenderer,
+  DrawingManager,
+  InfoWindow,
+  Marker,
+  OverlayView,
+  Polygon,
+  Polyline,
+  Rectangle,
+  SearchBox,
+} from "../index";
+
+describe("index", () => {
+  it("should be exported", () => {
+    expect(GoogleMap).toExist();
+
+    expect(Circle).toExist();
+    expect(DirectionsRenderer).toExist();
+    expect(DrawingManager).toExist();
+    expect(InfoWindow).toExist();
+    expect(Marker).toExist();
+    expect(OverlayView).toExist();
+    expect(Polygon).toExist();
+    expect(Polyline).toExist();
+    expect(Rectangle).toExist();
+    expect(SearchBox).toExist();
+  });
+});

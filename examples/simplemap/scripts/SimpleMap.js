d1ee9812c5d695572e15c2c843dd6b0e196f9cdd
Tom Chen <developer@tomchentw.com>
2015-10-13 10:58:25 +0800

    docs(simplemap): SimpleMap as React 0.14 stateless function components

--- examples/simplemap/scripts/SimpleMap.js
+++ examples/simplemap/scripts/SimpleMap.js
@@ -1,5 +1,4 @@
 import {default as React, Component} from "react";
-import {default as update} from "react-addons-update";
 
 import {default as GoogleMap} from "../../../src/GoogleMap";
 import {default as Marker} from "../../../src/Marker";
@@ -9,95 +8,30 @@ import {default as Marker} from "../../../src/Marker";
  * https://developers.google.com/maps/documentation/javascript/examples/event-arguments
  *
  * Add <script src="https://maps.googleapis.com/maps/api/js"></script> to your HTML to provide google.maps reference
+ *
+ * We use React 0.14 stateless function components here.
+ * https://facebook.github.io/react/blog/2015/09/10/react-v0.14-rc1.html#stateless-function-components
  */
-export default class SimpleMap extends Component {
-
-  state = {
-    markers: [{
-      position: {
-        lat: 25.0112183,
-        lng: 121.52067570000001,
-      },
-      key: "Taiwan",
-      defaultAnimation: 2,
-    }],
-  }
-
-  componentDidMount () {
-    setTimeout(() => {
-      var {markers} = this.state;
-      markers = update(markers, {
-        $push: [
-          {
-            position: {
-              lat: 25.99,
-              lng: 122.9,
-            },
-            defaultAnimation: 2,
-            key: Date.now(),// Add a key property for: http://fb.me/react-warning-keys
+export default function SimpleMap (props) {
+  return (
+    <section style={{height: "100%"}}>
+      <GoogleMap containerProps={{
+          style: {
+            height: "100%",
           },
-        ],
-      });
-      this.setState({ markers });
-    }, 2000);
-  }
-
-  /*
-   * This is called when you click on the map.
-   * Go and try click now.
-   */
-  _handle_map_click (event) {
-    var {markers} = this.state;
-    markers = update(markers, {
-      $push: [
-        {
-          position: event.latLng,
-          defaultAnimation: 2,
-          key: Date.now(),// Add a key property for: http://fb.me/react-warning-keys
-        },
-      ],
-    });
-    this.setState({ markers });
-
-    console.log(this.refs.map.getBounds());
-  }
-
-  _handle_marker_rightclick (index, event) {
-    /*
-     * All you modify is data, and the view is driven by data.
-     * This is so called data-driven-development. (And yes, it's now in
-     * web front end and even with google maps API.)
-     */
-    var {markers} = this.state;
-    markers = update(markers, {
-      $splice: [
-        [index, 1]
-      ],
-    });
-    this.setState({ markers });
-  }
-
-  render () {
-    return (
-      <section style={{height: "100%"}}>
-        <GoogleMap containerProps={{
-            style: {
-              height: "100%",
-            },
-          }}
-          ref="map"
-          defaultZoom={3}
-          defaultCenter={{lat: -25.363882, lng: 131.044922}}
-          onClick={::this._handle_map_click}>
-          {this.state.markers.map((marker, index) => {
-            return (
-              <Marker
-                {...marker}
-                onRightclick={this._handle_marker_rightclick.bind(this, index)} />
-            );
-          })}
-        </GoogleMap>
-      </section>
-    );
-  }
+        }}
+        defaultZoom={3}
+        defaultCenter={{lat: -25.363882, lng: 131.044922}}
+        onClick={props.onMapClick}
+      >
+        {props.markers.map((marker, index) => {
+          return (
+            <Marker
+              {...marker}
+              onRightclick={() => props.onMarkerRightclick(index)} />
+          );
+        })}
+      </GoogleMap>
+    </section>
+  );
 }

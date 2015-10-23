b4c9d93a56f628ac79eaacfc18e036aa867b240e
Tom Chen <developer@tomchentw.com>
2015-10-23 09:56:00 +0800

    docs(simplemap): remove old files

--- examples/simplemap/scripts/client.js
+++
@@ -1,6 +0,0 @@
-import {default as React} from "react";
-import {default as ReactDOM} from "react-dom";
-
-import {default as ReactRoot} from "./ReactRoot";
-
-ReactDOM.render(<ReactRoot />, document.getElementById("react-container"));

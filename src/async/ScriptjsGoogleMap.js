41fc73b04b946084de813d2163f277e259ea97d8
mattjstar <mstar@wework.com>
2015-11-17 16:29:03 -0500

    expose Public API's to ScriptjsGoogleMap

--- src/async/ScriptjsGoogleMap.js
+++ src/async/ScriptjsGoogleMap.js
@@ -33,6 +33,49 @@ export default class ScriptjsGoogleMap extends Component {
     loadingElement: PropTypes.node,
   }
 
+  // Public APIs
+  //
+  // https://developers.google.com/maps/documentation/javascript/3.exp/reference#Map
+  //
+  // [].map.call($0.querySelectorAll("tr>td>code"), function(it){ return it.textContent; }).filter(function(it){ return it.match(/^get/) && !it.match(/Map$/); })
+  getBounds () { return this.refs.map.getBounds(); }
+
+  getCenter () { return this.refs.map.getCenter(); }
+
+  getDiv () { return this.refs.map.getDiv(); }
+
+  getHeading () { return this.refs.map.getHeading(); }
+
+  getMapTypeId () { return this.refs.map.getMapTypeId(); }
+
+  getProjection () { return this.refs.map.getProjection(); }
+
+  getStreetView () { return this.refs.map.getStreetView(); }
+
+  getTilt () { return this.refs.map.getTilt(); }
+
+  getZoom () { return this.refs.map.getZoom(); }
+  // END - Public APIs
+  //
+  // https://developers.google.com/maps/documentation/javascript/3.exp/reference#Map
+  //
+  // Public APIs - Use this carefully
+  // See discussion in https://github.com/tomchentw/react-google-maps/issues/62
+  //
+  // https://developers.google.com/maps/documentation/javascript/3.exp/reference#Map
+  //
+  // [].map.call($0.querySelectorAll("tr>td>code"), function(it){ return it.textContent; }).filter(function(it){ return !it.match(/^get/) && !it.match(/^set/) && !it.match(/Map$/); })
+  fitBounds (bounds) { return this.refs.map.fitBounds(bounds); }
+
+  panBy (x, y) { return this.refs.map.panBy(x, y); }
+
+  panTo (latLng) { return this.refs.map.panTo(latLng); }
+
+  panToBounds (latLngBounds) { return this.refs.map.panToBounds(latLngBounds); }
+  // END - Public APIs - Use this carefully
+  //
+  // https://developers.google.com/maps/documentation/javascript/3.exp/reference#Map
+
   state = {
     isLoaded: false,
   }
@@ -52,14 +95,17 @@ export default class ScriptjsGoogleMap extends Component {
     const changedKeys = Object.keys(ScriptjsGoogleMap.propTypes)
       .filter(key => this.props[key] !== nextProps[key]);
 
-    warning(0 === changedKeys.length, `ScriptjsGoogleMap doesn't support mutating props after initial render. Changed props: %s`, `[${ changedKeys.join(", ") }]`);
+    // I'm disabling the warning because what if I pass an empty object into the query prop?
+    // This doesn't support object equality
+    //
+    // warning(0 === changedKeys.length, `ScriptjsGoogleMap doesn't support mutating props after initial render. Changed props: %s`, `[${ changedKeys.join(", ") }]`);
   }
 
   render () {
     if (this.state.isLoaded) {
       const {protocol, hostname, port, pathname, query, ...restProps} = this.props;
       return (
-        <GoogleMap {...restProps} />
+        <GoogleMap {...restProps} ref="map" />
       );
     } else {
       return this.props.loadingElement;

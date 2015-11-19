1426cf84e84a8fc83fe6f9e325b2fcdc63457609
Far Tseng <farrrr@gmail.com>
2015-11-19 10:27:51 +0800

    chore(gh-pages): add examples for MarkerClusterer
    * Ref #146

---
+++ examples/gh-pages/scripts/components/basics/MarkerClusterer.js
@@ -0,0 +1,48 @@
+import { default as React, Component } from "react";
+import { default as fetch } from "isomorphic-fetch";
+
+import { default as GoogleMap } from "../../../../../src/GoogleMap";
+import { default as Marker } from "../../../../../src/Marker";
+import { default as MarkerClusterer } from "../../../../../src/addons/MarkerClusterer";
+
+export default class MarkerClustererExample extends Component {
+  state = {
+    markers: [],
+  }
+
+  componentDidMount() {
+    fetch("https://gist.githubusercontent.com/farrrr/dfda7dd7fccfec5474d3/raw/758852bbc1979f6c4522ab4e92d1c92cba8fb0dc/data.json")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ markers: data.photos });
+      });
+  }
+
+  render() {
+    const { markers } = this.state;
+
+    return (
+      <GoogleMap
+        containerProps={{
+          ...this.props,
+          style: {
+            height: "100%",
+          },
+        }}
+        defaultZoom={ 3 }
+        defaultCenter={{ lat: 25.0391667, lng: 121.525 }}>
+          <MarkerClusterer
+           averageCenter={ true }
+           enableRetinaIcons={ true }
+           gridSize={ 60 }>
+           { markers.map(marker => (
+             <Marker
+               position={{ lat: marker.latitude, lng: marker.longitude }}
+               key={ marker.photo_id }
+             />
+           )) }
+       </MarkerClusterer>
+      </GoogleMap>
+    );
+  }
+}

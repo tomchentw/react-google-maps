353e67fd519f7fc26f8e7f522106ed44de49a8b7
Matt Furden <zolrath@gmail.com>
2016-05-07 14:33:05 -0700

    Add HeatmapLayer Addon
    Allows a HeatmapLayer to be created. Takes an array of
    `google.maps.LatLng` objects to the prop `data`.

---
+++ src/addons/HeatmapLayer.js
@@ -0,0 +1,61 @@
+import {
+  default as React,
+  Component,
+} from "react";
+
+import {
+  default as canUseDOM,
+} from "can-use-dom";
+
+import {
+  default as HeatmapLayerCreator,
+  heatmapLayerDefaultPropTypes,
+  heatmapLayerControlledPropTypes,
+  heatmapLayerEventPropTypes,
+} from "./addonsCreators/HeatmapLayerCreator";
+
+export default class HeatmapLayer extends Component {
+  static propTypes = {
+    // Uncontrolled default[props] - used only in componentDidMount
+    ...heatmapLayerDefaultPropTypes,
+    // Controlled [props] - used in componentDidMount/componentDidUpdate
+    ...heatmapLayerControlledPropTypes,
+    // Event [onEventName]
+    ...heatmapLayerEventPropTypes,
+  }
+
+  // Public APIs
+  //
+  // https://developers.google.com/maps/documentation/javascript/3.exp/reference#HeatmapLayer
+  //
+  // [].map.call($0.querySelectorAll("tr>td>code"), function(it){ return it.textContent; }).filter(function(it){ return it.match(/^get/) && !it.match(/^getMap/); })
+  getData() { return this.state.directionsRenderer.getDirections(); }
+
+  // END - Public APIs
+  //
+  // https://developers.google.com/maps/documentation/javascript/3.exp/reference#HeatmapLayer
+
+  state = {
+  }
+
+  componentWillMount() {
+    if (!canUseDOM) {
+      return;
+    }
+    const heatmapLayer = HeatmapLayerCreator._createHeatmapLayer(this.props);
+
+    this.setState({ heatmapLayer });
+  }
+
+  render() {
+    if (this.state.heatmapLayer) {
+      return (
+        <HeatmapLayerCreator heatmapLayer={this.state.heatmapLayer} {...this.props}>
+          {this.props.children}
+        </HeatmapLayerCreator>
+      );
+    } else {
+      return (<noscript />);
+    }
+  }
+}

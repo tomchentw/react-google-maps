c1ead72f47cf501873af8164bfbd571e15f97c2b
simek <gosimek@gmail.com>
2017-09-11 12:33:02 +0200

    use LatLngBounds instead of raw object, add basic OverlayView and GroundOverlay information to README

--- src/app/pages/basics/GroundOverlayExample.js
+++ src/app/pages/basics/GroundOverlayExample.js
@@ -16,14 +16,11 @@ const GroundOverlayExampleGoogleMap = withGoogleMap(props => (
     defaultCenter={{lat: 40.740, lng: -74.18}}
   >
     <GroundOverlay
-      clickable
       url={`https://www.lib.utexas.edu/maps/historical/newark_nj_1922.jpg`}
-      bounds={{
-        north: 40.773941,
-        south: 40.712216,
-        east: -74.12544,
-        west: -74.22655,
-      }}
+      bounds={new google.maps.LatLngBounds(
+        new google.maps.LatLng(40.712216, -74.22655),
+        new google.maps.LatLng(40.773941, -74.12544)
+      )}
       opacity={.5}
     />
   </GoogleMap>

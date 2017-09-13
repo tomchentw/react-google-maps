d647375e0406e5c0fd8fa78b0f829186f2877020
Tom Chen <developer@tomchentw.com>
2017-09-13 22:21:40 +0800

    chore(styleguide.config.js): add getComponentPathLine

---
+++ styleguide.config.js
@@ -0,0 +1,13 @@
+const _ = require("lodash")
+const path = require("path")
+
+module.exports = {
+  getComponentPathLine(componentPath) {
+    const name = path.basename(componentPath, ".jsx")
+    const dirname = path.basename(path.dirname(componentPath))
+    if (dirname === "components") {
+      return `import { ${name} } from "react-google-maps";`
+    }
+    return `import ${name} from "react-google-maps/lib/components/${dirname}/${name}";`
+  },
+}

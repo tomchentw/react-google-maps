df9bf340b96db60027f2447796d0fe05dae47880
Tom Chen <developer@tomchentw.com>
2017-09-13 22:21:40 +0800

    feat(places/SearchBox): revamp with jscodeshift
    BREAKING CHANGE: input element can now render directly as the only child
    
    Before:
    
    ```js
    <SearchBox
      inputPlaceholder="Customized your placeholder"
      inputStyle={{ padding: `0 12px`, fontSize: `14px`, outline: `none` }}
    />
    ```
    
    After:
    
    ```js
    <SearchBox>
      <input
        type="text"
        placeholder="Customized your placeholder"
        style={{
          padding: `0 12px`,
          fontSize: `14px`,
          outline: `none`,
        }}
      />
    </SearchBox>
    ```

---
+++ src/places/SearchBox.js
@@ -0,0 +1,8 @@
+import warning from "warning"
+
+warning(
+  false,
+  `[DEPRECATED] "react-google-maps/lib/places/SearchBox" has been moved to "react-google-maps/lib/components/places/SearchBox".`
+)
+
+export { default } from "../components/places/SearchBox"

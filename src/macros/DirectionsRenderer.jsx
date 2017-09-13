77e9253b3588d3a2a8b1cf03f5727ec105e28542
Tom Chen <developer@tomchentw.com>
2017-09-13 22:21:40 +0800

    feat(DirectionsRenderer): revamp with jscodeshift

---
+++ src/macros/DirectionsRenderer.jsx
@@ -0,0 +1,81 @@
+/* global google */
+import React from "react"
+import PropTypes from "prop-types"
+
+import {
+  construct,
+  componentDidMount,
+  componentDidUpdate,
+  componentWillUnmount,
+} from "../utils/MapChildHelper"
+
+import { MAP, DIRECTIONS_RENDERER } from "../constants"
+
+export const __jscodeshiftPlaceholder__ = `{
+  "eventMapOverrides": {
+  },
+  "getInstanceFromComponent": "this.state[DIRECTIONS_RENDERER]"
+}`
+
+/**
+ * @url https://developers.google.com/maps/documentation/javascript/3.exp/reference#DirectionsRenderer
+ */
+export class DirectionsRenderer extends React.PureComponent {
+  static propTypes = {
+    __jscodeshiftPlaceholder__: null,
+  }
+
+  static contextTypes = {
+    [MAP]: PropTypes.object,
+  }
+
+  /*
+   * @url https://developers.google.com/maps/documentation/javascript/3.exp/reference#DirectionsRenderer
+   */
+  constructor(props, context) {
+    super(props, context)
+    const directionsRenderer = new google.maps.DirectionsRenderer()
+    construct(
+      DirectionsRenderer.propTypes,
+      updaterMap,
+      this.props,
+      directionsRenderer
+    )
+    directionsRenderer.setMap(this.context[MAP])
+    this.state = {
+      [DIRECTIONS_RENDERER]: directionsRenderer,
+    }
+  }
+
+  componentDidMount() {
+    componentDidMount(this, this.state[DIRECTIONS_RENDERER], eventMap)
+  }
+
+  componentDidUpdate(prevProps) {
+    componentDidUpdate(
+      this,
+      this.state[DIRECTIONS_RENDERER],
+      eventMap,
+      updaterMap,
+      prevProps
+    )
+  }
+
+  componentWillUnmount() {
+    componentWillUnmount(this)
+    const directionsRenderer = this.state[DIRECTIONS_RENDERER]
+    if (directionsRenderer) {
+      directionsRenderer.setMap(null)
+    }
+  }
+
+  render() {
+    return false
+  }
+}
+
+export default DirectionsRenderer
+
+const eventMap = {}
+
+const updaterMap = {}

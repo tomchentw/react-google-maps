b92a570be58c6505fd4ccd4356c64020cdc888b6
Tom Chen <developer@tomchentw.com>
2017-09-13 22:21:40 +0800

    feat(visualization/HeatmapLayer): revamp with jscodeshift

---
+++ src/visualization/HeatmapLayer.js
@@ -0,0 +1,8 @@
+import warning from "warning"
+
+warning(
+  false,
+  `[DEPRECATED] "react-google-maps/lib/visualization/HeatmapLayer" has been moved to "react-google-maps/lib/components/visualization/HeatmapLayer".`
+)
+
+export { default } from "../components/visualization/HeatmapLayer"

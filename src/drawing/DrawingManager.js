5c3327e02fbc3c32766020b8b8b1a9e338a2662e
Tom Chen <developer@tomchentw.com>
2017-09-13 22:21:40 +0800

    feat(drawing/DrawingManager): revamp with jscodeshift

---
+++ src/drawing/DrawingManager.js
@@ -0,0 +1,8 @@
+import warning from "warning"
+
+warning(
+  false,
+  `[DEPRECATED] "react-google-maps/lib/drawing/DrawingManager" has been moved to "react-google-maps/lib/components/drawing/DrawingManager".`
+)
+
+export { default } from "../components/drawing/DrawingManager"

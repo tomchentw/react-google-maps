13cbef25eac1980125af79508deed5fed86b46f3
Tom Chen <developer@tomchentw.com>
2017-09-13 22:21:40 +0800

    feat(addons/MarkerClusterer): revamp to PureComponent

---
+++ src/addons/MarkerClusterer.js
@@ -0,0 +1,8 @@
+import warning from "warning"
+
+warning(
+  false,
+  `[DEPRECATED] "react-google-maps/lib/addons/MarkerClusterer" has been moved to "react-google-maps/lib/components/addons/MarkerClusterer"`
+)
+
+export { default } from "../components/addons/MarkerClusterer"

4f9811cf1296c1e790f0aafbf0e0f3b6d7d442e9
Tom Chen <developer@tomchentw.com>
2017-09-16 11:37:43 +0800

    chore(styleguide.config.js): add `showUsage: true`

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,6 +2,7 @@ const _ = require("lodash")
 const path = require("path")
 
 module.exports = {
+  showUsage: true,
   getComponentPathLine(componentPath) {
     const name = path.basename(componentPath, ".jsx")
     const dirname = path.basename(path.dirname(componentPath))

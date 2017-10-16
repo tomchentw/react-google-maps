9ee32bb3ee692330012a4261db48a61b5b72a8c7
Tom Chen <developer@tomchentw.com>
2017-10-16 23:20:09 +0800

    chore: remove visualization

--- src/visualization/HeatmapLayer.js
+++
@@ -1,8 +0,0 @@
-import warning from "warning"
-
-warning(
-  false,
-  `[DEPRECATED] "react-google-maps/lib/visualization/HeatmapLayer" has been moved to "react-google-maps/lib/components/visualization/HeatmapLayer".`
-)
-
-export { default } from "../components/visualization/HeatmapLayer"

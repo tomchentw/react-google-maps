5fca25b273df02f04a7601df85214a578ab91808
Tom Chen <developer@tomchentw.com>
2017-10-16 23:19:03 +0800

    chore: remove addons

--- src/addons/InfoBox.js
+++
@@ -1,8 +0,0 @@
-import warning from "warning"
-
-warning(
-  false,
-  `[DEPRECATED] "react-google-maps/lib/addons/InfoBox" has been moved to "react-google-maps/lib/components/addons/InfoBox"`
-)
-
-export { default } from "../components/addons/InfoBox"

0f4cb036bfa6dad039efb01e05a4a7cdf2e64f98
Tom Chen <developer@tomchentw.com>
2017-10-16 23:19:45 +0800

    chore: remove drawing

--- src/drawing/DrawingManager.js
+++
@@ -1,8 +0,0 @@
-import warning from "warning"
-
-warning(
-  false,
-  `[DEPRECATED] "react-google-maps/lib/drawing/DrawingManager" has been moved to "react-google-maps/lib/components/drawing/DrawingManager".`
-)
-
-export { default } from "../components/drawing/DrawingManager"

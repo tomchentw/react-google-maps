ae00454bb68d438726e02c145304f3192859abe4
Tom Chen <developer@tomchentw.com>
2017-10-16 23:19:57 +0800

    chore: remove places

--- src/places/SearchBox.js
+++
@@ -1,8 +0,0 @@
-import warning from "warning"
-
-warning(
-  false,
-  `[DEPRECATED] "react-google-maps/lib/places/SearchBox" has been moved to "react-google-maps/lib/components/places/SearchBox".`
-)
-
-export { default } from "../components/places/SearchBox"

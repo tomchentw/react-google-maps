0c8e94323ed8e42586a08e903b8c6ba504f857fe
Hemant Yadav <Hemant-yadav@users.noreply.github.com>
2017-12-14 16:56:37 +0530

    fix(places/SearchBox): undefined child at unmount
    Google Maps Api error can cause removal of searchBox which can results into an error at unmount while trying to append undefined to containerElement

--- src/macros/places/SearchBox.jsx
+++ src/macros/places/SearchBox.jsx
@@ -145,7 +145,9 @@ export class SearchBox extends React.PureComponent {
       const child = this.context[MAP].controls[
         this.props.controlPosition
       ].removeAt(this.mountControlIndex)
-      this.containerElement.appendChild(child)
+      if(child !== undefined){
+        this.containerElement.appendChild(child)
+      }
     }
   }
 
